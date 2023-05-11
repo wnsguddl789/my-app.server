f12e5a88e7b97756a446b3cb84638775c0f3164e
bbakjun <wnsguddl789@gmail.com>
2023-05-11 09:32:56 +0900

    ✨ : Email 모듈 생성 및 등록

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 
+// imports
+import { EmailModule } from '../email/email.module';
+
 // controllers
 import { UsersController } from './users.controller';
 
 // providers
 import { UsersService } from './users.service';
+	imports: [EmailModule],
 	controllers: [UsersController],
 export class UsersModule {}
