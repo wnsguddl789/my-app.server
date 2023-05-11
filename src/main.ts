8c7de7ce3cd219e094fd1c15c6a06f2dfb98afa0
bbakjun <wnsguddl789@gmail.com>
2023-05-11 16:18:36 +0900

    ✨ : 유효성검사 파이프라인 설정

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true,
+		}),
+	);
 	await app.listen(PORT);
 }
 bootstrap();
