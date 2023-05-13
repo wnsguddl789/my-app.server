37c1c66d44e99c7362a5c4fb0c93ad268b43cde1
BBAKJUN <wnsguddl789@gmail.com>
2023-05-13 13:43:01 +0900

    ✨ : TypeOrm 모듈 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
 // imports
@@ -10,6 +11,20 @@ import { validationSchema } from './config/validationSchema';
 @Module({
 	imports: [
 		UsersModule,
+		TypeOrmModule.forRoot({
+			type: 'mysql',
+			host: process.env.DATABASE_HOST || 'localhost',
+			port: 3306,
+			username: process.env.DATABASE_USERNAME || 'root',
+			password: process.env.DATABASE_PASSWORD || 'test',
+			database: process.env.DATABASE_NAME || 'my-app',
+			entities: [__dirname + '/**/*.entity{.ts,.js}'],
+			synchronize: false,
+			// synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
+			migrationsRun: false,
+			migrations: [__dirname + '/**/migrations/*.js'],
+			migrationsTableName: 'migrations',
+		}),
 		ConfigModule.forRoot({
 			envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
 			load: [configuration],
